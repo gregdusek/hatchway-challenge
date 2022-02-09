2beed1e5f15e6ed808045ee8cfdc3e0116d4594b
Greg Dusek <thegregdusek@gmail.com>
2022-02-09 13:47:51 -0600

    Setup more middleware & added controller file

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ require('dotenv').config();
 const express = require('express');
 const methodOverride = require('method-override');
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const axios = require('axios');
+const apicache = require('apicache');
+const cache = apicache.middleware; //allows requests to be cached
 
 //===============================
 //             PORT
@@ -18,11 +24,13 @@ const PORT = process.env.PORT || 3001;
 
 //use public folder for static assests
 app.use(express.static('public'));
-
 //populates req.body with parsed info from forms - if no data from forms will return an empty object {}
+
 app.use(express.urlencoded({ extended: false })); //extended false - does not allow nested objects in query strings
 app.use(express.json()); // returns middleware that only parses JSON - may or may not need it depending on your project
 app.use(methodOverride('_method')); //allow POST, PUT and DELETE from a form
+app.use(bodyParser.json()); // tells the system we want to use JSON
+app.use(morgan('dev')); //logs every request to the console
 
 //===============================
 //            ROUTES
